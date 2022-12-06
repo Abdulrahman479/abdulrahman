6b8902d2a2501f54dddd386a956c9930b249951b
abdulrahman <abdulrahmanmd479@gmail.com>
2022-12-06 10:06:59 +0000

    Create 4 files

---
+++ hello.js
@@ -0,0 +1,17 @@
+var a=46;
+let b=55;
+const c=23;
+for (let i = 0;i<10;i++){
+    console.log(i);
+    
+}
+const a={
+name: "abdulrahman",
+surname: "md",
+school:{
+
+name:"gms",
+    
+},    
+}
+const arr=[]
